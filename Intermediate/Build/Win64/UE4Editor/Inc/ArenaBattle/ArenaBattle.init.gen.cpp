ef0639ceb6cb49e713ae171672f3f0a52026f67f
PTHParkTaeHwan <pth0810@naver.com>
2020-03-11 17:34:58 +0900

    Npc Attack

--- Intermediate/Build/Win64/UE4Editor/Inc/ArenaBattle/ArenaBattle.init.gen.cpp
+++ Intermediate/Build/Win64/UE4Editor/Inc/ArenaBattle/ArenaBattle.init.gen.cpp
@@ -19,8 +19,8 @@ void EmptyLinkFunctionForGeneratedCodeArenaBattle_init() {}
 			static const UE4CodeGen_Private::FPackageParams PackageParams = {
 				"/Script/ArenaBattle",
 				PKG_CompiledIn | 0x00000000,
-				0x62FDEA62,
-				0x8DDB6853,
+				0x7BC522B2,
+				0x01B4EDDE,
 				nullptr, 0,
 				METADATA_PARAMS(nullptr, 0)
 			};
